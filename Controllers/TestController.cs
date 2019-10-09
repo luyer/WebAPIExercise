a7c6ac27accafd215ca5457b19471ed404afaa65
luis.guerra <luis.guerra@neoris.com>
2019-10-09 18:09:31 -0500

    diferentes ambientes

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -4,21 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace WebAPIExercise.Controllers
 {
 
+ 
+
+ 
     [ApiController]
     [Route("[controller]")]
+    public class TestController : ControllerBase
+    { 
 
-    public class TestController : ControllerBase{
         [HttpGet]
-        public string Get(){
+        public string Get(){        
             const string response = "Hola dude!";
-            return  response;
+            return response;
         }
 
-    }
+     }
+
+
+/*
+public class GreetingController : ControllerBase
+    {
+        public string appName { get; set; }
+
+        public GreetingController(IConfiguration config){
+            appName = config.GetValue<string>("applicationName");
+        }
+
+
+
+
+ */
+
+
 
 }
