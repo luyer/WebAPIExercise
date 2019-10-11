696e92e29748454e8abdea162554c6fc5fc71226
luis.guerra <luis.guerra@neoris.com>
2019-10-11 15:27:52 -0500

    Middleware excercice

--- Startup.cs
+++ Startup.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+// System.Threading.Tasks.Task
 
 namespace WebAPIExercise
 {
@@ -28,6 +31,21 @@ namespace WebAPIExercise
             services.AddControllers();
         }
 
+        private static void HandleMapHello(IApplicationBuilder app)
+        {
+            app.MapWhen(context => context.Request.Query.ContainsKey("name"),  HandleMapName);
+        }
+
+
+          private static void HandleMapName(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                string nameVar = context.Request.Query["name"];
+                await context.Response.WriteAsync("Hello "+nameVar);
+            });
+        }      
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -36,12 +54,12 @@ namespace WebAPIExercise
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Map("/hello", HandleMapHello);
+
             app.UseHttpsRedirection();
-
+            app.UseStaticFiles();
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
